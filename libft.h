c52ad77487078918a61055d381ed4d3eef189970
Alvaro Morion de eusebio <amorion-@c1r2s4.42madrid.com>
2021-07-31 13:05:07 +0200

    op

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amorion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 10:43:34 by amorion-          #+#    #+#             */
-/*   Updated: 2021/07/31 12:28:40 by amorion-         ###   ########.fr       */
+/*   Updated: 2021/07/31 13:04:11 by amorion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 # define LIBFT_H
 # include<stdlib.h>
 # include<unistd.h>
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
 int		ft_atoi(char *nptr);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
