12623e3080ad3e4fcc5be3a0447c24ee79bc10ce
Alvaro Morion de eusebio <amorion-@c1r2s2.42madrid.com>
2021-08-03 09:59:38 +0200

    v1

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,18 +6,28 @@
 /*   By: amorion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 19:41:41 by amorion-          #+#    #+#             */
-/*   Updated: 2021/08/02 09:34:23 by amorion-         ###   ########.fr       */
+/*   Updated: 2021/08/03 09:49:58 by amorion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_strlen_mod(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
 char	*ft_strdup(const char *s)
 {
 	int		i;
 	char	*dest;
 
-	dest = malloc(sizeof(*s));
+	dest = malloc(sizeof(char) * ft_strlen_mod(s) + 1);
 	if (!dest)
 		return (NULL);
 	i = 0;
